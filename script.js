47a5d1e9ced7910b0029338ea6855a1d2f6de86b
Jeff <jeffozordi@gmail.com>
2026-04-18 16:37:45 +0100

    search component

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-    
+const themeSwitch = document.getElementById('theme-switch');
+
+themeSwitch.addEventListener('click', function () {
+    this.classList.remove('fa-sun');
+    this.classList.add('fa-moon');
+
+    setTimeout(() => {
+        this.classList.remove('fa-moon');
+        this.classList.add('fa-sun');
+        this.style.color = '';
+    }, 1500);
+});
